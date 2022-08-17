311ad619835c3e403e2ffc4ee399e5fd64dc7ee8
Fernetazo <angelofdarkness88@gmail.com>
2022-08-17 18:59:21 -0300

    Add comment model

---
+++ models/comment.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema({
+  text: { type: String, require: true, maxLength: 100 },
+  timestamp: { type: Date, require: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  fatherPost: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+  upvotes: { type: Number, default: 1 },
+  downvotes: { type: Number, default: 0 },
+});
+
+//Virtual for comment's karma
+CommentSchema.virtual('karma').get(function () {
+  return this.upvotes - this.downvotes;
+});
+
+//Virtual for comment's URL
+CommentSchema.virtual('url').get(function () {
+  return '/comment/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Comment', CommentSchema);
