64c83a703fb77151dd82745d9971ff23d129703e
Fernetazo <angelofdarkness88@gmail.com>
2022-08-18 12:00:36 -0300

    Add routes (index file)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Require controller modules.
+var home_controller = require('../controllers/homeController');
+var post_controller = require('../controllers/postController');
+var message_controller = require('../controllers/messageController');
+var user_controller = require('../controllers/userController');
+
+// HOME
+router.route('/').get(home_controller.home_get);
+
+// POSTS ROUTES
+router
+  .route('/post/create')
+  .get(post_controller.post_create_get)
+  .post(post_controller.post_create_post);
+
+router.route('/post/:id').get(post_controller.post_read_get);
+
+router
+  .route('post/:id/edit')
+  .get(post_controller.post_edit_get)
+  .post(post_controller.post_edit_post);
+
+router
+  .route('post/:id/delete')
+  .get(post_controller.post_delete_get)
+  .post(post_controller.post_delete_post);
+
+// MESSAGE ROUTES
+router
+  .route('message/create')
+  .get(message_controller.message_create_get)
+  .post(message_controller.message_create_post);
+
+router
+  .route('message/:id/edit')
+  .get(message_controller.message_edit_get)
+  .post(message_controller.message_edit_post);
+
+router
+  .route('message/:id/delete')
+  .get(message_controller.message_delete_get)
+  .post(message_controller.message_delete_post);
+
+// USER ROUTES
+router
+  .route('user/login')
+  .get(user_controller.user_login_get)
+  .post(user_controller.user_login_post);
+
+router.route('/user/logout').get(user_controller.user_logout_get);
+
+router
+  .route('user/signup')
+  .get(user_controller.user_signup_get)
+  .post(user_controller.user_signup_post);
+
+router.route('user/:id').get(user_controller.user_profile_get);
 
 module.exports = router;
