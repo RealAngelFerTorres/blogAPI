85860a5f762016d4a3cd423ee62afb7c302ad60a
Fernetazo <angelofdarkness88@gmail.com>
2023-04-04 14:14:56 -0300

    Deploy app to fly.io

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static('build'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Cors.
